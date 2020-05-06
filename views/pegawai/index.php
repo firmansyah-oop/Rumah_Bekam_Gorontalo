5e3cca48b1007e0ca11e56ee2b572c7074f762a9
firmansyah-oop <62861543+firmansyah-oop@users.noreply.github.com>
2020-05-07 07:24:23 +0800

    ViewtambahData

--- views/pegawai/index.php
+++ views/pegawai/index.php
@@ -1,50 +1,52 @@
- <!-- DataTables Example -->
-  <div class="card shadow mb-4" style="max-height: 700; z-index: 1;">
+ <div class="container-fluid" style="z-index: 99">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Daftar Pegawai
-        <a href="<?= base_url('Pegawai/tambahData');?>" class="btn btn-primary btn-sm float-right">
-            + Tambah Data
-        </a>
+        <a href="<?= base_url('pegawai'); ?>"><i class="fa fa-backspace mx-3 float-left my-0" title="Daftar Pegawai"></i></a>
+        <h6 class="m-0 font-weight-bold text-primary">Tambah Data Pegawai
+        
     </div>
-        <?= $this->session->flashdata('tambah') ?>
-        <?= $this->session->flashdata('hapus') ?>
-        <?= $this->session->flashdata('ubah') ?>
-    <div class="card-body">
-      <div class="table-responsive">
-        <table class="table table-border shadow" id="dataTable" width="100%" cellspacing="0">
-          <thead>
-    <!-- <thead class="thead-light"> -->
-        <tr style="text-align: center">
-            <th scope="col">No</th>
-            <th scope="col">Nama Pegawai</th>
-            <th scope="col">NIP</th>
-            <th scope="col">Jabatan</th>
-            <th scope="col" colspan="">Aksi</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php $i = 1;
-        foreach ($pegawai as $pegawai) : ?>
-            <tr style="text-align: center">
-                <th scope="row"><?= $i; ?></th>
-                <td><?= $pegawai['nama_pegawai']; ?></td>
-                <td><?= $pegawai['nip']; ?></td>
-                <td><?= $pegawai['jabatan']; ?></td>
-                <td>
-                    <span title="Hapus" onclick="return confirm('Yakin akan dihapus?');"><?php echo anchor('pegawai/hapus/'. $pegawai['id_pegawai'], ' <div class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></div> ');?>  
-                    </span>
-                    <span title="Edit"><?php echo anchor('pegawai/ubahData/'. $pegawai['id_pegawai'],'<div class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></div></td>');?>
-                    </span>
-                </td>
-            </tr>
-        <?php $i++;
-        endforeach ?>
-    </tbody>
-</table>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
+	<hr>
+	<br>
+
+	<form action="<?= base_url(); ?>pegawai/prosesTambahData" method="POST" autocomplete="off" >
+		<div class="ml-10">
+
+		<input type="hidden" name="id_pasien" id="id_pasien">
+
+        <div class="form-group row">
+            <label for="nama_pegawai" class="col-sm-2 col-form-table my-2">Nama Pegawai</label>
+            <div class="my-2">:</div>	
+            <div class="col-sm-5">
+	            <input type="text" class="form-control" id="nama_pegawai" name="nama_pegawai" required>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="nip" class="col-sm-2 col-form-table my-2">NIP</label>
+            <div class="my-2">:</div>
+            <div class="col-sm-5">
+	            <input type="text" class="form-control" id="nip" name="nip" required>
+	        </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="jabatan" class="col-sm-2 col-form-table my-2">Jabatan</label>
+            <div class="my-2 mr-3">:</div>
+            <select class="form-control col-sm-4" id="jabatan" name="jabatan" required>
+                <option value="Manager">Manager</option>
+                <option value="Terapis">Terapis</option>
+                <option value="Customer Service">Customer Service</option>
+            </select>
+        </div>
+
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-table my-2"></label>
+            <div class="col-sm-5">
+                <button type="submit" class="btn btn-primary">Tambah</button>
+                <button type="reset" class="btn btn-danger">Batal</button>
+            </div>
+        </div>
+
+    </div>
+	</form>
+
 </div>
