31144007ce4563a41427d2142b4ecafb7a33c127
firmansyah-oop <62861543+firmansyah-oop@users.noreply.github.com>
2020-05-07 05:24:58 +0800

    fungsi ubah-hapus ON

--- controllers/Pasien.php
+++ controllers/Pasien.php
@@ -46,26 +46,58 @@ class Pasien extends CI_controller
 		redirect('pasien');
 	}
 
-	public function ubahData()
-	{ 
-		$where = array('id_pasien' =>['id_pasien']);
+	public function hapus($id_pasien)
+	{
+		$where= array('id_pasien' =>$id_pasien);
+		$this->m_pasien->hapusData($where, 'tb_pasien');
+		$this->session->set_flashdata('hapus', '<div class="alert alert-success" role="alert"> Data Berhasil Dihapus</div>');
+		redirect('pasien');
+	}
+
+
+	public function ubahData($id_pasien)
+	{
+	 // tampilan ubah data
+		$where = array('id_pasien' => $id_pasien);
 			
 		$data['pasien'] = $this->m_pasien->ubahData($where, 'tb_pasien')->result();
-
-		// tampilan setelah di ubah data
 		$id_role =$this->db->get_where('user', ['username' => $this->session->userdata('id_role')])->row_array();
-        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
-        $data['icon'] = 'fas fa-user-tie';
-        $data['judul'] = 'Daftar Pasien';
+		$data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+		$data['icon'] = 'fas fa-user-tie';
+		$data['judul'] = 'Daftar Pasien';
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('pasien/ubah', $data);
-        $this->load->view('templates/footer');
+		$this->load->view('templates/header', $data);
+		$this->load->view('pasien/ubah', $data);
+		$this->load->view('templates/footer');
 
 	}
 
 	public function prosesUbahData()
 	{
+		$id_pasien = $this->input->post('id_pasien');
+		$tgl_regis_pasien = $this->input->post('tgl_regis_pasien');
+		$nama_pasien = $this->input->post('nama_pasien');
+		$nik = $this->input->post('nik');
+		$jk = $this->input->post('jk');
+		$umur = $this->input->post('umur');
+		$no_hp = $this->input->post('no_hp');
+		$pekerjaan = $this->input->post('pekerjaan');
+		$alamat = $this->input->post('alamat');
 
+		$data = array(
+			'id_pasien' => $id_pasien,
+			'tgl_regis_pasien' => $tgl_regis_pasien,
+			'nama_pasien' =>$nama_pasien,
+			'nik' => $nik,
+			'jk' => $jk,
+			'umur' => $umur,
+			'no_hp' => $no_hp,
+			'pekerjaan' => $pekerjaan,
+			'alamat' => $alamat
+		);
+		$where = [ 'id_pasien' =>$id_pasien];
+		$this->m_pasien->prosesUbahData($where, $data, 'tb_pasien');
+		$this->session->set_flashdata('ubah', '<div class="alert alert-success" role="alert"> Data Berhasil Diubah</div>');
+		redirect('pasien');
 	}
 }
