eddb4bfdf48ab7ae36f52b60cb3ca6e2d12ea107
firmansyah-oop <62861543+firmansyah-oop@users.noreply.github.com>
2020-05-07 07:28:23 +0800

    fungsi ubah-hapus pegawai ON

--- controllers/Pegawai.php
+++ controllers/Pegawai.php
@@ -49,58 +49,70 @@ class Pegawai extends CI_Controller
         redirect('pegawai');
 
     }
+    
+    public function tambahData()
+    {
+        $id_role =$this->db->get_where('user', ['username' => $this->session->userdata('id_role')])->row_array();
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->model('m_pegawai');
+        $data['pegawai'] = $this->m_pegawai->get_data();
+        $data['icon'] = 'fas fa-user-tie';
+        $data['judul'] = 'Daftar Pegawai';
 
-    // public function tambah()
-    // {
-    //     if ($this->model('Pegawai_model')->tambahDataPegawai($_POST) > 0) {
-    //         Flasher::setFlash('Data Pegawai', 'BERHASIL', 'Ditambahkan', 'success');
-    //         header('location: ' . BASEURL . '/pegawai');
-    //         exit;
-    //     } else {
-    //         Flasher::setFlash('Data Pegawai',  'GAGAL', 'Ditambahkan', 'danger');
-    //         header('location: ' . BASEURL . '/pegawai');
-    //         exit;
-    //     }
-    // }
+        $this->load->view('templates/header', $data);
+        $this->load->view('pegawai/tambah', $data);
+        $this->load->view('templates/footer');
+    }
 
-    // public function hapus($id_pegawai)
-    // {
-    //     if ($this->model('Pegawai_model')->hapusDataPegawai($id_pegawai) > 0) {
-    //         Flasher::setFlash('Data Pegawai', 'BERHASIL', 'Dihapus', 'success');
-    //         header('location: ' . BASEURL . '/pegawai');
-    //         exit;
-    //     } else {
-    //         Flasher::setFlash('Data Pegawai',  'GAGAL', 'Dihapus', 'danger');
-    //         header('location: ' . BASEURL . '/pegawai');
-    //         exit;
-    //     }
-    // }
+    public function prosesTambahData()
+    {
+        $this->m_pegawai->prosesTambahData();
+        $this->session->set_flashdata('tambah', '<div class="alert alert-success" role="alert"> Data Berhasil Ditambahkan</div>');
+        redirect('pegawai');
 
-    // public function getUbah()
-    // {
-    //     echo json_encode($this->model('Pegawai_model')->getPegawaiById($_POST['id_pegawai']));
-    // }
+    }
 
-    // public function ubah()
-    // {
-    //     if ($this->model('Pegawai_model')->ubahDataPegawai($_POST) > 0) {
-    //         Flasher::setFlash('Data Pegawai', 'Berhasil', 'Diubah', 'success');
-    //         header('Location: ' . BASEURL . '/pegawai');
-    //         exit;
-    //     } else {
-    //         Flasher::setFlash('Data Pegawai', 'Gagal', 'Diubah', 'danger');
-    //         header('Location: ' . BASEURL . '/pegawai');
-    //         exit;
-    //     }
-    // }
+    public function hapus($id_pegawai)
+    {
+        $where= array('id_pegawai' =>$id_pegawai);
+        $this->m_pegawai->hapusData($where, 'tb_pegawai');
+        $this->session->set_flashdata('hapus', '<div class="alert alert-success" role="alert"> Data Berhasil Dihapus</div>');
+        redirect('pegawai');
+    }
 
-    // public function cari()
-    // {
-    //     $data['icon'] = 'fas fa-user-tie';
-    //     $data['judul'] = 'Daftar Pegawai';
-    //     $data['pegawai'] = $this->model('Pegawai_model')->cariDataPegawai();
-    //     $this->view('templates/header', $data);
-    //     $this->view('pegawai/index', $data);
-    //     $this->view('templates/footer');
-    // }
+    public function ubahData($id_pegawai)
+    {
+     // tampilan ubah data
+        $where = array('id_pegawai' => $id_pegawai);
+            
+        $data['pegawai'] = $this->m_pegawai->ubahData($where, 'tb_pegawai')->result();
+        $id_role =$this->db->get_where('user', ['username' => $this->session->userdata('id_role')])->row_array();
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $data['icon'] = 'fas fa-user-tie';
+        $data['judul'] = 'Daftar Pegawai';
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('pegawai/ubah', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function prosesUbahData()
+    {
+        $id_pegawai = $this->input->post('id_pegawai');
+        $nama_pegawai = $this->input->post('nama_pegawai');
+        $nip = $this->input->post('nip');
+        $jabatan = $this->input->post('jabatan');
+
+        $data = array(
+            'id_pegawai' => $id_pegawai,
+            'nama_pegawai' => $nama_pegawai,
+            'nip' =>$nip,
+            'jabatan' => $jabatan
+            
+        );
+        $where = [ 'id_pegawai' =>$id_pegawai];
+        $this->m_pegawai->prosesUbahData($where, $data, 'tb_pegawai');
+        $this->session->set_flashdata('ubah', '<div class="alert alert-success" role="alert"> Data Berhasil Diubah</div>');
+        redirect('pegawai');
+    }
 }
